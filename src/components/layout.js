88f0315ab21e3c61b1c4d06ab5ed892226631644
Morgan Baker <morganbakerdesigner@gmail.com>
2019-08-24 23:25:46 +0100

    Added fonts and sass

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,8 @@
 import React from "react"
 import Header from "../components/header"
 import Footer from "../components/footer"
+import "typeface-roboto"
+import "../scss/main.scss"
 
 export default ({ children }) => (
     <>
