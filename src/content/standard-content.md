ca55d112e27afd621cdfbab344505aff6060d230
Morgan Baker <morganbakerdesigner@gmail.com>
2019-08-25 22:33:15 +0100

    featured images added to posts

--- src/content/standard-content.md
+++ src/content/standard-content.md
@@ -2,6 +2,7 @@
 title: Standard Content
 date: 2019-08-26
 path: /standard-content
+featuredImage: ./images/dark-design.jpg
 ---
 
 Standard page content that can be used with this starter.
--- src/templates/post.js
+++ src/templates/post.js
@@ -1,15 +1,18 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
+import Img from "gatsby-image"
 
 export default function Template({ data }) {
     const { markdownRemark } = data // Object destructuring
     const { frontmatter, html } = markdownRemark // Object destructuring og markdownRemark
+    let featuredImgFluid = markdownRemark.frontmatter.featuredImage.childImageSharp.fluid
 
     return (
         <Layout>
             <main className="blog-post">
                 <h1>{frontmatter.title}</h1>
+                <Img fluid={featuredImgFluid} />
                 <h2>{frontmatter.date}</h2>
                 <div className="blog-post-content" dangerouslySetInnerHTML={{ __html: html }} />
             </main>
@@ -24,6 +27,13 @@ export const pageQuery = graphql`
                 date(formatString: "MMMM DD, YYYY")
                 path
                 title
+                featuredImage {
+                    childImageSharp {
+                      fluid(maxWidth: 800) {
+                        ...GatsbyImageSharpFluid
+                      }
+                    }
+                  }
             }
         }
     }
