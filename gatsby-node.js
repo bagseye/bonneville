88527783617f3baf0ba9effe6a6ebb451260a15e
Morgan Baker <morganbakerdesigner@gmail.com>
2019-08-26 13:54:04 +0100

    Blog list and pagination added

--- gatsby-node.js
+++ gatsby-node.js
@@ -55,4 +55,21 @@ exports.createPages = async ({ actions, graphql }) => {
             component: path.resolve(`src/templates/post.js`),
         })
     })
+
+    // Create blog list pages
+    const posts = result.data.allMarkdownRemark.edges
+    const postsPerPage = 1
+    const numPages = Math.ceil(posts.length / postsPerPage)
+    Array.from({ length: numPages }).forEach((_, i) => {
+        createPage({
+            path: i === 0 ? `/blog` : `/blog/${i + 1}`,
+            component: path.resolve("./src/templates/blog-list-template.js"),
+            context: {
+                limit: postsPerPage,
+                skip: i * postsPerPage,
+                numPages,
+                currentPage: i + 1,
+            },
+        })
+    })
 }
