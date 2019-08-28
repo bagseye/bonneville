7ca5667f4140c1ff0832bb59b2735e870f31fef9
Morgan Baker <morganbakerdesigner@gmail.com>
2019-08-28 22:28:54 +0100

    disable gatsby plugin offline

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,7 @@ module.exports = {
       },
     },
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-offline`,
+    // `gatsby-plugin-offline`,
     `gatsby-transformer-remark`,
     {
       resolve: `gatsby-source-filesystem`,
