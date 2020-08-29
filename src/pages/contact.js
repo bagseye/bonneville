09b3964aba31a27d2ad704fd04698c994f79af7b
Morgan Baker <morganbakerdesigner@gmail.com>
2020-08-29 13:53:05 +0100

    Styling updates to contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,18 +4,36 @@ import SEO from "../components/SEO"
 import styled from "styled-components"
 import Button from "../components/Button/button"
 
+const Form = styled.div`
+  margin: calc(var(--spacing) * 2) 0;
+
+  @media (min-width: 1200px) {
+    margin: calc(var(--spacing) * 4) 0;
+  }
+`
+
 const FormLabel = styled.label`
   font-family: var(--serif);
-  font-size: var(--h3);
-  margin-bottom: 0.75rem;
+  font-size: var(--h5);
+  margin-bottom: var(--spacing);
   display: inline-block;
 `
 
 const FormInput = styled.input`
   height: 50px;
   width: 100%;
-  margin-bottom: 1.25rem;
-  font-size: 1rem;
+  margin-bottom: calc(var(--spacing) * 1.5);
+  font-size: var(--para);
+  padding: 10px;
+  box-sizing: border-box;
+  border: 1px solid rgba(0, 0, 0, 0.25);
+`
+
+const FormTextArea = styled.textarea`
+  width: 100%;
+  min-height: 150px;
+  margin-bottom: calc(var(--spacing) * 1.5);
+  font-size: var(--para);
   padding: 10px;
   box-sizing: border-box;
   border: 1px solid rgba(0, 0, 0, 0.25);
@@ -39,19 +57,25 @@ const ContactPage = () => {
           nec, dignissim erat. Sed eu consequat neque, interdum malesuada eros.
           Ut maximus gravida malesuada.{" "}
         </p>
-        <form name="contact" netlify>
-          <p>
-            <FormLabel for="name">Name</FormLabel>
-            <FormInput type="text" name="name" />
-          </p>
-          <p>
-            <FormLabel for="email">Email</FormLabel>
-            <FormInput type="email" name="email" />
-          </p>
-          <p>
-            <Button text="Send Message" type="submit" />
-          </p>
-        </form>
+        <Form>
+          <form name="contact" netlify>
+            <p>
+              <FormLabel for="name">Your Name</FormLabel>
+              <FormInput type="text" name="name" />
+            </p>
+            <p>
+              <FormLabel for="email">Email Address</FormLabel>
+              <FormInput type="email" name="email" />
+            </p>
+            <p>
+              <FormLabel for="message">Your Message</FormLabel>
+              <FormTextArea type="text" name="message" />
+            </p>
+            <p>
+              <Button className="btn-link" text="Send Message" type="submit" />
+            </p>
+          </form>
+        </Form>
       </Layout>
     </>
   )
