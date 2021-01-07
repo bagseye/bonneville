b7789ce855546dd75fee9a160266e7167747d4c4
Morgan Baker <morganbakerdesigner@gmail.com>
2021-01-07 09:06:09 +0000

    fixes for layout component

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,5 +1,5 @@
 import React from "react"
-import Layout from "./src/components/Layout"
+import Layout from "./src/components/layout"
 import { MenuProvider } from "./src/components/MenuContext"
 
 export function wrapPageElement({ element, props }) {
