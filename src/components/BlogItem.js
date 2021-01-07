1dfc54f05e08f56de523623a9f822d99de4b99e6
Morgan Baker <morganbakerdesigner@gmail.com>
2021-01-07 00:08:53 +0000

    Update to footer component

--- src/components/BlogItem.js
+++ src/components/BlogItem.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
-import Button from "./Button/button"
+import Button from "./Button"
 import Img from "gatsby-image"
 import styled from "styled-components"
 import PropTypes from "prop-types"
