135271615b0bee83a1a5911f1eeebe9310265080
Morgan Baker <morganbakerdesigner@gmail.com>
2021-01-07 00:22:53 +0000

    layout fixes

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,5 @@
 import React from "react"
+// import Layout from "./src/components/Layout"
 import Layout from "./src/components/Layout"
 import { MenuProvider } from "./src/components/MenuContext"
 
