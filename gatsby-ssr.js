357e92bb72b65913af4b4d2a8c0e9ebc2c15919a
Morgan Baker <morganbakerdesigner@gmail.com>
2021-01-07 08:29:22 +0000

    Change layout to sfc

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,5 +1,4 @@
 import React from "react"
-// import Layout from "./src/components/Layout"
 import Layout from "./src/components/Layout"
 import { MenuProvider } from "./src/components/MenuContext"
 
