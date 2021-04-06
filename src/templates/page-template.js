4bcceafa9985e099c9cf6dea57fbc49f438d5f10
Morgan Baker <morganbakerdesigner@gmail.com>
2021-04-06 08:28:51 +0100

    Fix case of context

--- src/templates/page-template.js
+++ src/templates/page-template.js
@@ -2,16 +2,16 @@ import React from "react"
 import Banner from "../components/Banner"
 import Seo from "../components/SEO"
 
-const PageTemplate = ({pageCOntext: {page}}) => {
-  return ( 
+const PageTemplate = ({ pageContext: { page } }) => {
+  return (
     <>
-    <Seo title={page.name} description={page.title} />
-    <div className="page-standard">
-      <Banner content={page.title} />
-      <p>{page.content}</p>
-    </div>
-  </>
-   );
+      <Seo title={page.name} description={page.title} />
+      <div className="page-standard">
+        <Banner content={page.title} />
+        <p>{page.content}</p>
+      </div>
+    </>
+  )
 }
- 
-export default PageTemplate;
+
+export default PageTemplate
